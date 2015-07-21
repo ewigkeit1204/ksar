ba909e0c1668331557596ebf2be069f22a5faca0
Keisuke Kamada <ewigkeit1204@gmail.com>
2015-07-21 14:11:23 +0900

    Improve date format processing.
    Convert date formats to enum parameter.
    Add a format.

--- src/main/java/net/atomique/ksar/Config.java
+++ src/main/java/net/atomique/ksar/Config.java
@@ -6,14 +6,21 @@ package net.atomique.ksar;
 
 import java.awt.Font;
 import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Properties;
 import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
+
 import javax.swing.UIManager;
+
+import net.atomique.ksar.Parser.Linux;
 
 /**
  *
@@ -21,220 +28,237 @@ import javax.swing.UIManager;
  */
 public class Config {
 
-    private static Preferences myPref;
-    private static Config instance = new Config();
+	private static Preferences myPref;
+	private static Config instance = new Config();
 
-    public static Config getInstance() {
-        return instance;
-    }
+	public static Config getInstance() {
+		return instance;
+	}
 
-    Config() {
-        myPref = Preferences.userNodeForPackage(Config.class);
-        if (myPref.getInt("local_configfile", -1) == -1) {
-            // new
-            try {
-                myPref.clear();
-                myPref.flush();
-            } catch (BackingStoreException e) {
-            }
-            local_configfile = store_configdir();
-            myPref.putInt("local_configfile", local_configfile);
-            
-        }
-        load();
-    }
+	Config() {
+		myPref = Preferences.userNodeForPackage(Config.class);
+		if (myPref.getInt("local_configfile", -1) == -1) {
+			// new
+			try {
+				myPref.clear();
+				myPref.flush();
+			} catch (BackingStoreException e) {
+			}
+			local_configfile = store_configdir();
+			myPref.putInt("local_configfile", local_configfile);
 
-    private static void load() {
-        /*
-         * load default value or stored value
-         */
-        setLandf(myPref.get("landf", UIManager.getLookAndFeel().getName()));
-        setLastReadDirectory(myPref.get("lastReadDirectory", null));
-        setLastExportDirectory(myPref.get("lastExportDirectory", null));
+		}
+		load();
+	}
 
-        setImageHeight(myPref.getInt("ImageHeight", 600));
-        setImageWidth(myPref.getInt("ImageWidth", 800));
-        setPDFPageFormat(myPref.get("PDFPageFormat","A4"));
-        setLinuxDateFormat(myPref.get("LinuxDateFormat","Always ask"));
+	private static void load() {
+		/*
+		 * load default value or stored value
+		 */
+		setLandf(myPref.get("landf", UIManager.getLookAndFeel().getName()));
+		setLastReadDirectory(myPref.get("lastReadDirectory", null));
+		setLastExportDirectory(myPref.get("lastExportDirectory", null));
 
-        setNumber_host_history(myPref.getInt("HostHistory", 0));
-        for (int i = 0; i < getNumber_host_history(); i++) {
-            host_history.add(myPref.get("HostHistory_" + i, null));
-        }
-        setLocal_configfile(myPref.getInt("local_configfile", -1));
-    }
+		setImageHeight(myPref.getInt("ImageHeight", 600));
+		setImageWidth(myPref.getInt("ImageWidth", 800));
+		setPDFPageFormat(myPref.get("PDFPageFormat", "A4"));
 
-    public static void save() {
-        if (myPref == null) {
-            return;
-        }
-        myPref.put("landf", landf);
-        if (lastReadDirectory != null) {
-            myPref.put("lastReadDirectory", lastReadDirectory.toString());
-        }
-        if (lastExportDirectory != null) {
-            myPref.put("lastExportDirectory", lastExportDirectory.toString());
-        }
+		byte[] buf = myPref.getByteArray("LinuxDateFormat", null);
+		if (buf == null) {
+			setLinuxDateFormat(Linux.DateFormat.ALWAYS_ASK);
+		} else {
+			try (ByteArrayInputStream bais = new ByteArrayInputStream(buf);
+					ObjectInputStream ois = new ObjectInputStream(bais)) {
+				Linux.DateFormat format = (Linux.DateFormat) ois.readObject();
+				setLinuxDateFormat(format);
+			} catch (ClassNotFoundException e) {
+				throw new IllegalStateException(e);
+			} catch (IOException e) {
+				// ignore
+			}
+		}
 
-        myPref.putInt("ImageHeight", ImageHeight);
-        myPref.putInt("ImageWidth", ImageWidth);
-        myPref.put("PDFPageFormat", PDFPageFormat);
-        myPref.put("LinuxDateFormat", LinuxDateFormat);
+		setNumber_host_history(myPref.getInt("HostHistory", 0));
+		for (int i = 0; i < getNumber_host_history(); i++) {
+			host_history.add(myPref.get("HostHistory_" + i, null));
+		}
+		setLocal_configfile(myPref.getInt("local_configfile", -1));
+	}
 
-        for (int i = 0; i < host_history.size(); i++) {
-            myPref.put("HostHistory_" + i, host_history.get(i));
-        }
-        myPref.putInt("HostHistory", host_history.size());
+	public static void save() {
+		if (myPref == null) {
+			return;
+		}
+		myPref.put("landf", landf);
+		if (lastReadDirectory != null) {
+			myPref.put("lastReadDirectory", lastReadDirectory.toString());
+		}
+		if (lastExportDirectory != null) {
+			myPref.put("lastExportDirectory", lastExportDirectory.toString());
+		}
 
-        myPref.putInt("local_configfile", local_configfile);
+		myPref.putInt("ImageHeight", ImageHeight);
+		myPref.putInt("ImageWidth", ImageWidth);
+		myPref.put("PDFPageFormat", PDFPageFormat);
 
-    }
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try (ObjectOutputStream oos = new ObjectOutputStream(baos)) {
+			oos.writeObject(LinuxDateFormat);
+		} catch (IOException e) {
+			// ignore
+		}
+		myPref.putByteArray("LinuxDateFormat", baos.toByteArray());
 
-    public static String getLandf() {
-        return landf;
-    }
+		for (int i = 0; i < host_history.size(); i++) {
+			myPref.put("HostHistory_" + i, host_history.get(i));
+		}
+		myPref.putInt("HostHistory", host_history.size());
 
-    public static void setLandf(String landf) {
-        Config.landf = landf;
-    }
+		myPref.putInt("local_configfile", local_configfile);
 
-    public static File getLastReadDirectory() {
-        return lastReadDirectory;
-    }
+	}
 
-    public static void setLastReadDirectory(String lastReadDirectory) {
-        if (lastReadDirectory != null) {
-            Config.lastReadDirectory = new File(lastReadDirectory);
-        }
-    }
+	public static String getLandf() {
+		return landf;
+	}
 
-    public static void setLastReadDirectory(File lastReadDirectory) {
-        Config.lastReadDirectory = lastReadDirectory;
-    }
+	public static void setLandf(String landf) {
+		Config.landf = landf;
+	}
 
-    public static File getLastExportDirectory() {
-        return lastReadDirectory;
-    }
+	public static File getLastReadDirectory() {
+		return lastReadDirectory;
+	}
 
-    public static void setLastExportDirectory(String lastExportDirectory) {
-        if (lastExportDirectory != null) {
-            Config.lastExportDirectory = new File(lastExportDirectory);
-        }
-    }
+	public static void setLastReadDirectory(String lastReadDirectory) {
+		if (lastReadDirectory != null) {
+			Config.lastReadDirectory = new File(lastReadDirectory);
+		}
+	}
 
-    public static void setLastExportDirectory(File lastExportDirectory) {
-        Config.lastExportDirectory = lastExportDirectory;
-    }
+	public static void setLastReadDirectory(File lastReadDirectory) {
+		Config.lastReadDirectory = lastReadDirectory;
+	}
 
-    public static String getLastCommand() {
-        return lastCommand;
-    }
+	public static File getLastExportDirectory() {
+		return lastReadDirectory;
+	}
 
-    public static void setLastCommand(String lastCommand) {
-        Config.lastCommand = lastCommand;
-    }
+	public static void setLastExportDirectory(String lastExportDirectory) {
+		if (lastExportDirectory != null) {
+			Config.lastExportDirectory = new File(lastExportDirectory);
+		}
+	}
 
-    public static ArrayList<String> getHost_history() {
-        return host_history;
-    }
+	public static void setLastExportDirectory(File lastExportDirectory) {
+		Config.lastExportDirectory = lastExportDirectory;
+	}
 
-    public static void addHost_history(String e) {
-        host_history.add(e);
-    }
+	public static String getLastCommand() {
+		return lastCommand;
+	}
 
-    public static int getNumber_host_history() {
-        return number_host_history;
-    }
+	public static void setLastCommand(String lastCommand) {
+		Config.lastCommand = lastCommand;
+	}
 
-    public static void setNumber_host_history(int number_host_history) {
-        Config.number_host_history = number_host_history;
-    }
+	public static ArrayList<String> getHost_history() {
+		return host_history;
+	}
 
-    public static Font getDEFAULT_FONT() {
-        return DEFAULT_FONT;
-    }
+	public static void addHost_history(String e) {
+		host_history.add(e);
+	}
 
-    public static int getImageHeight() {
-        return ImageHeight;
-    }
+	public static int getNumber_host_history() {
+		return number_host_history;
+	}
 
-    public static void setImageHeight(int ImageHeight) {
-        Config.ImageHeight = ImageHeight;
-    }
+	public static void setNumber_host_history(int number_host_history) {
+		Config.number_host_history = number_host_history;
+	}
 
-    public static int getImageWidth() {
-        return ImageWidth;
-    }
+	public static Font getDEFAULT_FONT() {
+		return DEFAULT_FONT;
+	}
 
-    public static void setImageWidth(int ImageWidth) {
-        Config.ImageWidth = ImageWidth;
-    }
+	public static int getImageHeight() {
+		return ImageHeight;
+	}
 
-    public static String getPDFPageFormat() {
-        return PDFPageFormat;
-    }
+	public static void setImageHeight(int ImageHeight) {
+		Config.ImageHeight = ImageHeight;
+	}
 
-    public static void setPDFPageFormat(String PDFPageFormat) {
-        Config.PDFPageFormat = PDFPageFormat;
-    }
+	public static int getImageWidth() {
+		return ImageWidth;
+	}
 
-    
+	public static void setImageWidth(int ImageWidth) {
+		Config.ImageWidth = ImageWidth;
+	}
 
-    public static int store_configdir() {
-        Properties systemprops = System.getProperties();
-        String userhome = (String) systemprops.get("user.home") + systemprops.get("file.separator");
-        String username = (String) systemprops.get("user.name");
-        String fileseparator = (String) systemprops.get("file.separator");
-        // mkdir userhome/.ksar
-        String buffer = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\n\n<ConfiG>\n</ConfiG>\n";
-        boolean home = new File(userhome + ".ksarcfg").mkdir();
-        if (!home) {
-            return 0;
-        }
+	public static String getPDFPageFormat() {
+		return PDFPageFormat;
+	}
 
-        BufferedWriter out = null;
-        try {
-            out = new BufferedWriter(new FileWriter(userhome + ".ksarcfg" + fileseparator + "Config.xml"));
-            out.write(buffer);
-            out.flush();
-            out.close();
-            return 1;
-        } catch (IOException e) {
-            return 0;
-        }
+	public static void setPDFPageFormat(String PDFPageFormat) {
+		Config.PDFPageFormat = PDFPageFormat;
+	}
 
-    }
+	public static int store_configdir() {
+		Properties systemprops = System.getProperties();
+		String userhome = (String) systemprops.get("user.home") + systemprops.get("file.separator");
+		String username = (String) systemprops.get("user.name");
+		String fileseparator = (String) systemprops.get("file.separator");
+		// mkdir userhome/.ksar
+		String buffer = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\n\n<ConfiG>\n</ConfiG>\n";
+		boolean home = new File(userhome + ".ksarcfg").mkdir();
+		if (!home) {
+			return 0;
+		}
 
-    public static int getLocal_configfile() {
-        return local_configfile;
-    }
+		BufferedWriter out = null;
+		try {
+			out = new BufferedWriter(new FileWriter(userhome + ".ksarcfg" + fileseparator + "Config.xml"));
+			out.write(buffer);
+			out.flush();
+			out.close();
+			return 1;
+		} catch (IOException e) {
+			return 0;
+		}
 
-    public static void setLocal_configfile(int local_configfile) {
-        Config.local_configfile = local_configfile;
-    }
+	}
 
-    public static String getLinuxDateFormat() {
-        return LinuxDateFormat;
-    }
+	public static int getLocal_configfile() {
+		return local_configfile;
+	}
 
-    public static void setLinuxDateFormat(String LinuxDateFormat) {
-        Config.LinuxDateFormat = LinuxDateFormat;
-    }
+	public static void setLocal_configfile(int local_configfile) {
+		Config.local_configfile = local_configfile;
+	}
 
-    
-    
-    private static String landf;
-    private static File lastReadDirectory;
-    private static File lastExportDirectory;
-    private static String lastCommand;
-    private static int number_host_history;
-    private static int local_configfile;
-    private static ArrayList<String> host_history = new ArrayList<String>();
-    public static final Font DEFAULT_FONT = new Font("SansSerif", Font.BOLD, 18);
+	public static Linux.DateFormat getLinuxDateFormat() {
+		return LinuxDateFormat;
+	}
 
-    private static String LinuxDateFormat;
-    private static String PDFPageFormat;
-    private static int ImageWidth;
-    private static int ImageHeight;
-    
+	public static void setLinuxDateFormat(Linux.DateFormat LinuxDateFormat) {
+		Config.LinuxDateFormat = LinuxDateFormat;
+	}
+
+	private static String landf;
+	private static File lastReadDirectory;
+	private static File lastExportDirectory;
+	private static String lastCommand;
+	private static int number_host_history;
+	private static int local_configfile;
+	private static ArrayList<String> host_history = new ArrayList<String>();
+	public static final Font DEFAULT_FONT = new Font("SansSerif", Font.BOLD, 18);
+
+	private static Linux.DateFormat LinuxDateFormat;
+	private static String PDFPageFormat;
+	private static int ImageWidth;
+	private static int ImageHeight;
+
 }
--- src/main/java/net/atomique/ksar/Parser/Linux.java
+++ src/main/java/net/atomique/ksar/Parser/Linux.java
@@ -6,16 +6,18 @@ package net.atomique.ksar.Parser;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.Locale;
+import java.util.function.Consumer;
+
+import org.jfree.data.time.Second;
+
 import net.atomique.ksar.Config;
-import net.atomique.ksar.OSParser;
 import net.atomique.ksar.GlobalOptions;
+import net.atomique.ksar.OSParser;
 import net.atomique.ksar.Graph.Graph;
 import net.atomique.ksar.Graph.List;
 import net.atomique.ksar.UI.LinuxDateFormat;
 import net.atomique.ksar.XML.GraphConfig;
-import org.jfree.data.time.Second;
 
 /**
  *
@@ -23,185 +25,225 @@ import org.jfree.data.time.Second;
  */
 public class Linux extends OSParser {
 
-    public void parse_header(String s) {
-        boolean retdate = false;
-        LinuxDateFormat = Config.getLinuxDateFormat();
-        String[] columns = s.split("\\s+");
-        String tmpstr;
-        setOstype(columns[0]);
-        setKernel(columns[1]);
-        tmpstr = columns[2];
-        setHostname(tmpstr.substring(1, tmpstr.length() - 1));
-        checkDateFormat();
-        retdate=setDate(columns[3]);
-        
-    }
+	public static enum DateFormat {
 
-    private void checkDateFormat() {
+		// Always ask
+		ALWAYS_ASK("Always ask", t -> {
+			t.askDateFormat("Provide date Format");
+			t.checkDateFormat();
+		}),
 
-        if ("Always ask".equals(LinuxDateFormat)) {
-            askDateFormat("Provide date Format");
-        }
-        
-        if ("MM/DD/YYYY 23:59:59".equals(LinuxDateFormat)) {
-            dateFormat = "MM/dd/yy";
-        } else if ("MM/DD/YYYY 12:59:59 AM|PM".equals(LinuxDateFormat)) {
-            dateFormat = "MM/dd/yy";
-            timeFormat = "hh:mm:ss a";
-            timeColumn=2;
-        } else if ("DD/MM/YYYY 23:59:59".equals(LinuxDateFormat)) {
-            dateFormat = "dd/MM/yy";
-        } else if ("YYYY-MM-DD 23:59:59".equals(LinuxDateFormat)) {
-            dateFormat = "yy-MM-dd";
-        }  
-    }
+		// MM/DD/YYYY 23:59:59
+		MM_DD_YYYY_24H("MM/DD/YYYY 23:59:59", t -> t.dateFormat = "MM/dd/yy"),
 
-    private void askDateFormat(String s) {
-        if ( GlobalOptions.hasUI() ) {
-            LinuxDateFormat tmp = new LinuxDateFormat(GlobalOptions.getUI(),true);
-            tmp.setTitle(s);
-            if ( tmp.isOk()) {
-                LinuxDateFormat=tmp.getDateFormat();
-                if ( tmp.hasToRemenber() ) {
-                    Config.setLinuxDateFormat(tmp.getDateFormat());
-                    Config.save();
-                }
-            }
-        }
-    }
-    
-    @Override
-    public int parse(String line, String[] columns) {
-        int heure = 0;
-        int minute = 0;
-        int seconde = 0;
-        Second now = null;
+		// MM/DD/YYYY 12:59:59 AM|PM
+		MM_DD_YYYY_12H("MM/DD/YYYY 12:59:59 AM|PM", t -> {
+			t.dateFormat = "MM/dd/yy";
+			t.timeFormat = "HH:mm:ss a";
+			t.timeColumn = 2;
+		}),
 
-        if ("Average:".equals(columns[0])) {
-            currentStat = "NONE";
-            return 0;
-        }
+		// DD/MM/YYYY 23:59:59
+		DD_MM_YYYY_24H("DD/MM/YYYY 23:59:59", t -> t.dateFormat = "dd/MM/yy"),
 
-        if (line.indexOf("unix restarts") >= 0 || line.indexOf(" unix restarted") >= 0) {
-            return 0;
-        }
+		// YYYY-MM-DD 23:59:59
+		YYYY_MM_DD_24H("YYYY-MM-DD 23:59:59", t -> t.dateFormat = "yyyy-MM-dd"),
 
-        // match the System [C|c]onfiguration line on AIX
-        if (line.indexOf("System Configuration") >= 0 || line.indexOf("System configuration") >= 0) {
-            return 0;
-        }
+		// YYYY-MM-DD 12:59:59 AM|PM
+		YYYY_MM_DD_12H("YYYY-MM-DD 12:59:59 AM|PM", t -> {
+			t.dateFormat = "yyyy-MM-dd";
+			t.timeFormat = "HH:mm:ss a";
+			t.timeColumn = 2;
+		});
 
-        if (line.indexOf("State change") >= 0) {
-            return 0;
-        }
+		String description;
+		Consumer<Linux> func;
 
-        try {
-            if ( timeColumn == 2 ) {
-                parsedate = new SimpleDateFormat(timeFormat).parse(columns[0]+" "+columns[1]);
-            } else {
-                parsedate = new SimpleDateFormat(timeFormat).parse(columns[0]);
-            }
-            cal.setTime(parsedate);
-            heure = cal.get(cal.HOUR_OF_DAY);
-            minute = cal.get(cal.MINUTE);
-            seconde = cal.get(cal.SECOND);
-            now = new Second(seconde, minute, heure, day, month, year);
-            if (startofstat == null) {
-                startofstat = now;
-                startofgraph = now;
-            }
-            if (endofstat == null) {
-                endofstat = now;
-                endofgraph = now;
-            }
-            if (now.compareTo(endofstat) > 0) {
-                endofstat = now;
-                endofgraph = now;
-            }
-            firstdatacolumn = timeColumn;
-        } catch (ParseException ex) {
-            System.out.println("unable to parse time " + columns[0]);
-            return -1;
-        }
+		DateFormat(String description, Consumer<Linux> func) {
+			this.description = description;
+			this.func = func;
+		}
 
+		private void accept(Linux t) {
+			func.accept(t);
+		}
 
-        //00:20:01     CPU  i000/s  i001/s  i002/s  i008/s  i009/s  i010/s  i011/s  i012/s  i014/s
-        if ("CPU".equals(columns[firstdatacolumn]) && line.matches(".*i([0-9]+)/s.*")) {
-            currentStat = "IGNORE";
-            return 1;
-        }
-        /** XML COLUMN PARSER **/
-        String checkStat = myosconfig.getStat(columns, firstdatacolumn);
-        if (checkStat != null) {
-            Object obj = ListofGraph.get(checkStat);
-            if (obj == null) {
-                GraphConfig mygraphinfo = myosconfig.getGraphConfig(checkStat);
-                if (mygraphinfo != null) {
-                    if ("unique".equals(mygraphinfo.getType())) {
-                        obj = new Graph(mysar, mygraphinfo, mygraphinfo.getTitle(), line, firstdatacolumn, mysar.graphtree);
+		/**
+		 * @see java.lang.Enum#toString()
+		 */
+		@Override
+		public String toString() {
+			return description;
+		}
 
-                        ListofGraph.put(checkStat, obj);
-                        currentStat = checkStat;
-                        return 0;
-                    }
-                    if ("multiple".equals(mygraphinfo.getType())) {
-                        obj = new List(mysar, mygraphinfo, mygraphinfo.getTitle(), line, firstdatacolumn);
+	}
 
-                        ListofGraph.put(checkStat, obj);
-                        currentStat = checkStat;
-                        return 0;
-                    }
-                } else {
-                    // no graph associate
-                    currentStat = checkStat;
-                    return 0;
-                }
-            } else {
-                currentStat = checkStat;
-                return 0;
-            }
-        }
+	private DateFormat format;
 
-        //System.out.println( currentStat +" " + line);
+	public void parse_header(String s) {
+		boolean retdate = false;
+		format = Config.getLinuxDateFormat();
+		String[] columns = s.split("\\s+");
+		String tmpstr;
+		setOstype(columns[0]);
+		setKernel(columns[1]);
+		tmpstr = columns[2];
+		setHostname(tmpstr.substring(1, tmpstr.length() - 1));
+		checkDateFormat();
+		retdate = setDate(columns[3]);
 
+	}
 
+	private void checkDateFormat() {
+		format.accept(this);
+	}
 
-        if (lastStat != null) {
-            if (!lastStat.equals(currentStat) ) {
-                if (  GlobalOptions.isDodebug())  {
-                System.out.println("Stat change from " + lastStat + " to " + currentStat);
-                }
-                lastStat = currentStat;
-            }
-        } else {
-            lastStat = currentStat;
-        }
-        
-        if ("IGNORE".equals(currentStat)) {
-            return 1;
-        }
-        if ("NONE".equals(currentStat)) {
-            return -1;
-        }
+	private void askDateFormat(String s) {
+		if (GlobalOptions.hasUI()) {
+			LinuxDateFormat tmp = new LinuxDateFormat(GlobalOptions.getUI(), true);
+			tmp.setTitle(s);
+			if (tmp.isOk()) {
+				format = tmp.getDateFormat();
+				if (tmp.hasToRemenber()) {
+					Config.setLinuxDateFormat(tmp.getDateFormat());
+					Config.save();
+				}
+			}
+		}
+	}
 
-        currentStatObj = ListofGraph.get(currentStat);
-        if (currentStatObj == null) {
-            return -1;
-        } else {
-            DateSamples.add(now);
-            if (currentStatObj instanceof Graph) {
-                Graph ag = (Graph) currentStatObj;
-                return ag.parse_line(now, line);
-            }
-            if (currentStatObj instanceof List) {
-                List ag = (List) currentStatObj;
-                return ag.parse_line(now, line);
-            }
-        }
-        return -1;
-    }
+	@Override
+	public int parse(String line, String[] columns) {
+		int heure = 0;
+		int minute = 0;
+		int seconde = 0;
+		Second now = null;
 
+		if ("Average:".equals(columns[0])) {
+			currentStat = "NONE";
+			return 0;
+		}
 
-    private String LinuxDateFormat;
-    
+		if (line.indexOf("unix restarts") >= 0 || line.indexOf(" unix restarted") >= 0) {
+			return 0;
+		}
+
+		// match the System [C|c]onfiguration line on AIX
+		if (line.indexOf("System Configuration") >= 0 || line.indexOf("System configuration") >= 0) {
+			return 0;
+		}
+
+		if (line.indexOf("State change") >= 0) {
+			return 0;
+		}
+
+		try {
+			if (timeColumn == 2) {
+				parsedate = new SimpleDateFormat(timeFormat, Locale.ENGLISH).parse(columns[0] + " " + columns[1]);
+			} else {
+				parsedate = new SimpleDateFormat(timeFormat, Locale.ENGLISH).parse(columns[0]);
+			}
+			cal.setTime(parsedate);
+			heure = cal.get(cal.HOUR_OF_DAY);
+			minute = cal.get(cal.MINUTE);
+			seconde = cal.get(cal.SECOND);
+			now = new Second(seconde, minute, heure, day, month, year);
+			if (startofstat == null) {
+				startofstat = now;
+				startofgraph = now;
+			}
+			if (endofstat == null) {
+				endofstat = now;
+				endofgraph = now;
+			}
+			if (now.compareTo(endofstat) > 0) {
+				endofstat = now;
+				endofgraph = now;
+			}
+			firstdatacolumn = timeColumn;
+		} catch (ParseException ex) {
+			if (timeColumn == 2) {
+				System.out.println("unable to parse time " + columns[0] + " " + columns[1]);
+			} else {
+				System.out.println("unable to parse time " + columns[0]);
+			}
+			return -1;
+		}
+
+		// 00:20:01 CPU i000/s i001/s i002/s i008/s i009/s i010/s i011/s i012/s
+		// i014/s
+		if ("CPU".equals(columns[firstdatacolumn]) && line.matches(".*i([0-9]+)/s.*")) {
+			currentStat = "IGNORE";
+			return 1;
+		}
+		/** XML COLUMN PARSER **/
+		String checkStat = myosconfig.getStat(columns, firstdatacolumn);
+		if (checkStat != null) {
+			Object obj = ListofGraph.get(checkStat);
+			if (obj == null) {
+				GraphConfig mygraphinfo = myosconfig.getGraphConfig(checkStat);
+				if (mygraphinfo != null) {
+					if ("unique".equals(mygraphinfo.getType())) {
+						obj = new Graph(mysar, mygraphinfo, mygraphinfo.getTitle(), line, firstdatacolumn,
+								mysar.graphtree);
+
+						ListofGraph.put(checkStat, obj);
+						currentStat = checkStat;
+						return 0;
+					}
+					if ("multiple".equals(mygraphinfo.getType())) {
+						obj = new List(mysar, mygraphinfo, mygraphinfo.getTitle(), line, firstdatacolumn);
+
+						ListofGraph.put(checkStat, obj);
+						currentStat = checkStat;
+						return 0;
+					}
+				} else {
+					// no graph associate
+					currentStat = checkStat;
+					return 0;
+				}
+			} else {
+				currentStat = checkStat;
+				return 0;
+			}
+		}
+
+		// System.out.println( currentStat +" " + line);
+
+		if (lastStat != null) {
+			if (!lastStat.equals(currentStat)) {
+				if (GlobalOptions.isDodebug()) {
+					System.out.println("Stat change from " + lastStat + " to " + currentStat);
+				}
+				lastStat = currentStat;
+			}
+		} else {
+			lastStat = currentStat;
+		}
+
+		if ("IGNORE".equals(currentStat)) {
+			return 1;
+		}
+		if ("NONE".equals(currentStat)) {
+			return -1;
+		}
+
+		currentStatObj = ListofGraph.get(currentStat);
+		if (currentStatObj == null) {
+			return -1;
+		} else {
+			DateSamples.add(now);
+			if (currentStatObj instanceof Graph) {
+				Graph ag = (Graph) currentStatObj;
+				return ag.parse_line(now, line);
+			}
+			if (currentStatObj instanceof List) {
+				List ag = (List) currentStatObj;
+				return ag.parse_line(now, line);
+			}
+		}
+		return -1;
+	}
+
 }
